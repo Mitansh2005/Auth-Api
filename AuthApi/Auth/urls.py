aa047066e5ec8e8f2d525b80efba4c5d5158779b
Kairav Pithadia <kkp@Kairavs-MacBook-Pro.local>
2024-02-25 18:06:57 +0530

    feat: dockerize code

--- AuthApi/Auth/urls.py
+++ AuthApi/Auth/urls.py
@@ -1,9 +1,21 @@
-from django.urls import path,include
-from Auth.views import  UserRegisteration,UserLogin,UserProfileView,ChangeUserPassword,UserChangePasswordEmailView
+from django.urls import path
+from Auth.views import (
+    Root,
+    UserRegisteration,
+    UserLogin,
+    UserProfileView,
+    ChangeUserPassword,
+    UserChangePasswordEmailView,
+)
+
 urlpatterns = [
-    path('register/', UserRegisteration.as_view(),name='register'),
-    path('login/', UserLogin.as_view(),name='login'),
-    path('Userprofile/',UserProfileView.as_view(),name="profile"),
-    path('changepassword/',ChangeUserPassword.as_view(),name="change_password"),
-    path('send-reset-password-email/',UserChangePasswordEmailView.as_view(),name="rest-email"),
+    path("register/", UserRegisteration.as_view(), name="register"),
+    path("login/", UserLogin.as_view(), name="login"),
+    path("Userprofile/", UserProfileView.as_view(), name="profile"),
+    path("changepassword/", ChangeUserPassword.as_view(), name="change_password"),
+    path(
+        "send-reset-password-email/",
+        UserChangePasswordEmailView.as_view(),
+        name="rest-email",
+    ),
 ]
